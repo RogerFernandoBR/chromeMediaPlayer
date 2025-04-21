dd3846791fa0133f9b98f13dcb29fcfd15164dd5
Roger Carvalho <rogerfernandodeoliveira@gmail.com>
2025-04-20 23:27:02 -0300

    Alterado upload video

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { IUploadInterface } from 'src/app/interfaces/_interfaces';
 
 @Component({
   selector: 'app-video',
@@ -9,6 +10,14 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class VideoComponent {
   prev_url : any;
 
+  uploadObj: IUploadInterface = {
+    text: "Arraste sua midia aqui ou clique para procurar!",
+    icon: {
+      name: "image-plus",
+      size: "90",
+      stroke: 0.5
+    }
+  }
   constructor(
     private sanitizer : DomSanitizer
   ) {}
@@ -17,5 +26,29 @@ export class VideoComponent {
     let file = ev.target.files[0];
     var URL = window.URL;
     this.prev_url = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file));
+  }
+
+  triggerUpload() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput?.click();
+  }
+  
+  onDragOver(event: DragEvent) {
+    event.preventDefault();  // Impede o comportamento padrão (como abrir a midia no navegador)
+    event.stopPropagation();  // Impede que o evento se propague
+  }
+  
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    const file = event.dataTransfer?.files[0]; // Obtém o arquivo arrastado
+    if (file?.type.startsWith('video/')) {
+      const video = document.createElement('video');
+      video.autoplay = false;
+      video.muted = true;
+      video.src = URL.createObjectURL(file);  
+      this.prev_url = this.sanitizer.bypassSecurityTrustUrl(video.src);
+    }
   }
 }
